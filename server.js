4081c6b77e0161b30603622f4937f633029ceaf1
Nadja Brandys <nadiusza@gmail.com>
2020-07-05 22:05:32 +0200

    Added new endpoint, bug

--- server.js
+++ server.js
@@ -1,52 +1,26 @@
 const express = require('express');
-const path = require('path');
-const { uuid } = require('uuidv4');
-const app = express();
+const cors = require('cors');
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
+const app = express();
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+app.use('/api/', testimonialsRoutes);
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
 
 
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  ];
-
-app.get('/testimonials', (req, res) => {
-    res.json(db);
-  });
-
-  app.get('/testimonials/:id', (req, res) => {
-    res.json(db[Math.floor(Math.random() * db.length)]);
-  });
-
-  app.get('/testimonials/random', (req, res) => {
-    res.json(db[req.params.id - 1]);
-  });
-
-  app.post('/testimonials', (req, res) => {
-    const { author, text } = req.body;
-    const id =uuid();
-    res.json({ message: 'OK' });
-  });
-
-  app.put('/testimonials/:id', (req,res) =>{
-const { author, text } = req.body;
-res.json({ message: 'OK' });
-  })
-
-  app.delete('/testimonials/:id', (req, res) => {
-    res.json({ message: 'OK' });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
-  });
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+});
 
 app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+  console.log('Server is running on port: 8000');
+});
 
 
