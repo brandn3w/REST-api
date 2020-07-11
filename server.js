25f4eedb0632b315a35b6bde67a46f6a59576df8
Nadja Brandys <nadiusza@gmail.com>
2020-07-11 18:34:21 +0200

    working static heroku-local

--- server.js
+++ server.js
@@ -9,32 +9,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//STATIC
 app.use(cors());
-
-app.use(express.static(path.join(__dirname, '/client/build/index.html')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
-
-// if (process.env.NODE_ENV === "production") {
-//   // get directory where is index.html
-//   const root = path.join(__dirname, 'client', 'build');
-//   //express.use static with the directory
-//   app.use(express.static(root));
-//   //express get request any (*) root, please use file that is on root directory configure above.
-//   app.get("*", (req, res) => {
-//     res.sendFile('index.html', { root });
-//   });
-
-//}
-// else {
-//   app.use(express.static(path.join(__dirname, '/client/build/index.html')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build/index.html'));
-//   });
-// }
+//STATIC
+  // get directory where is index.html
+const root = path.join(__dirname, 'client', 'build');
+  //express.use static with the directory
+app.use(express.static(root));
+  //express get request any (*) root, please use file that is on root directory configure above.
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+  });
 
 //Dynamic Api
 
