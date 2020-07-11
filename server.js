7572d6cf1a15bc01fac1c4fcde961b353bca4351
Nadja Brandys <nadiusza@gmail.com>
2020-07-11 18:01:57 +0200

    deploy app to heroku static not working

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './client/build/index.html')));
+
+const root = path.join(__dirname, 'client', 'build');
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', {root});
+});
 
 app.use(cors());
 app.use('/api/', testimonialsRoutes);
